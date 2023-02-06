a9cf8dada122295688c3ec3141e67658e5cb9155
Шугыла <220648@astanait.edu.kz>
2023-02-06 14:22:35 +0600

    user

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,5 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Ilkham");
-        System.out.println("ioioioio");
-        System.out.println(" hgghfdhgd");
-
     }
 }
