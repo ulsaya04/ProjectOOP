bfc3dc4c7f7562ae28f64f4965cb7b90ed4b52ed
Alexey <asimovilhamzhan@gmail.com>
2023-02-06 09:54:16 +0600

    gre

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Ilkham");
+        int name = 10 ;
         System.out.println(" kak vy ");
 
     }
