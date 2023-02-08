ab348eac444602373cd5cb49429f9d0aa5cd08f4
UlsayaPC <asimovilhamzhan@gmail.com>
2023-02-08 20:02:34 +0600

    another

--- src/Registration.java
+++ src/Registration.java
@@ -3,8 +3,37 @@ import java.util.Scanner;
 public class Registration extends User{
 static Scanner in = new Scanner(System.in);
 
-public Registration(string username,
+public Registration(String username,
                     String password, String position) {
     super(username, password, position);
 }
-public static void reg()}
+public static void reg() throws
+SQLException {
+    User user = new User();
+    System.out.println("Write username: ");
+    String username = in.next();
+    user.setUsername(username);
+    System.out.println("Write password: ");
+    String pass = in.next();
+    user.setPassword(checkPass(pass));
+    System.out.println("Choose position: 1) seller or 2) buyer");
+    int position = in.nextInt();
+    switch (position) {
+        case 1 -> user.setPosition("seller");
+        case 2 -> user.setPosition("buyer");
+    }
+    user.insert();
+    System.out.println("You have successfully registered!");
+    Main.mainMenu();
+}
+private static String checkPass(String pass) {
+    if (!checkCorrrectPassword(pass)) {
+        System.out.println("Your password is weak! It should contain both large and small letters and numbers");
+        String password = in.next();
+        checkPass(password);
+    }
+    return pass;
+}
+
+}
+
