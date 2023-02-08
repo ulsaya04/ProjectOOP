66c61ffcc3abc4be297846610f121e673d2c82a5
UlsayaPC <asimovilhamzhan@gmail.com>
2023-02-08 19:41:07 +0600

    ert

--- src/Registration.java
+++ src/Registration.java
@@ -1,2 +1,10 @@
-public class Registration {
+import java.sql.SQLException;
+import java.util.Scanner;
+public class Registration extends User{
+static Scanner in = new Scanner(System.in);
+
+public Registration(string username,
+                    String password, String position) {
+    super(username, password, position);
 }
+public static void reg()}
