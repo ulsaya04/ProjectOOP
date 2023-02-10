5281add7819c4b1ce6fb1c7a66f85bbaade0ed40
Alexey <asimovilhamzhan@gmail.com>
2023-02-10 12:52:33 +0600

    test15

--- src/Customer.java
+++ src/Customer.java
@@ -1,3 +1,20 @@
-public class Customer {
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Customer extends  User {
+    static Scanner in = new Scanner(System.in);
+    public Customer(String username, String password, String fole, Double balance ){
+        super(username,password,role,balance);
+    }
+    public  Customer(){}
+    @Override
+    return " Name:"+ getUsername()+", balance:"+ getBalance()+  " tenge, role: " + getRole();
+}
+public  static void buy() throws  SQLException{
+    System.out.println("Write the gadget  ID");
+    int id = in.nextInt();
 
 }
--- src/Seller.java
+++ src/Seller.java
@@ -1,21 +1,45 @@
 import java.sql.SQLException;
 import java.util.Scanner;
+import java.sql.Connection;
 
 public class Seller extends User {
     static Scanner in = new Scanner(System.in);
     public Seller (String username, String password, String position){
+
         super (username, password, position);
     }
     public Seller(){}
     @Override
     public String toString(){
+
         return "Name: " + getUsername() + ", position: " + getPosition();
     }
     public static void addGadget() throws SQLException{
         Gadget gadget = new Gadget(name, price, category);
         System.out.println("Write the name of the gadget: ");
         String name = in.next();
+        gadget.setName(name);
+        System.out.println("Write the price of the gadget:");
         double price = in.nextDouble();
         gadget.setPrice(price);
+        System.out.println("Write the product category:");
+        Gadget.infoCategories();
+        int x=in.nextInt();
+        switch (x){
+            case 1 ->gadget.setCategory("Mobile Phones" );
+            case 2 ->gadget.setCategory("Laptop");
+            case 3 ->gadget.setCategory("Smart Watch");
+            case 4 ->gadget.setCategory("Headphones");
+        }
+        gadget.insert();
+        Gadget.seachProcedure(gadget.getName(), gadget.getPrice(), gadget.getCategory());
+        Main.forTheSeller();
+    }
+    public void deleteGadget() throws  SQLException{
+        System.out.println("Write the ID of the gadget you want to delete");
+        int id = in.nextInt();
+        dropMovie(id);
+        System.out.println("You have successfully deleted the gadget!");
+        Main.forTheSeller();
     }
 }
